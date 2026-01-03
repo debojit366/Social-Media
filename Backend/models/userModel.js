dd20f58d34f55040de19334a11f989a2a4712346
debojit366 <debojitdas366@gmail.com>
2026-01-03 22:55:06 +0530

    feat : created userModel

---
+++ Backend/models/userModel.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose'
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 30,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Male", "Female", "Others"],
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
