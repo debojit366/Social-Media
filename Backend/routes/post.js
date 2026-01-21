0c7f6bfa9ee8eee7b0b31b75c7e6bc3c08b8ac9e
debojit366 <debojitdas366@gmail.com>
2026-01-21 18:51:41 +0530

    feat(routes): add timeline route to fetch user timeline posts

--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,14 +1,16 @@
 import express from "express";
-import { createPost, updatePost, deletePost, getPost, likePost,getUserProfilePosts } from "../controllers/postController.js";
+import { createPost, updatePost, deletePost, getPost, likePost,getUserProfilePosts,getTimelinePosts } from "../controllers/postController.js";
 import verifyToken from "../middleware/verifyToken.js";
 import { upload } from "../config/cloudinary.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, upload.single("img"), createPost);
+router.get("/timeline", verifyToken, getTimelinePosts);
 router.get("/user-posts/:id", verifyToken, getUserProfilePosts);
 router.put("/:id", verifyToken,upload.single("img"), updatePost);
 router.delete("/:id", verifyToken, deletePost);
 router.get("/:id", getPost);
 router.put("/:id/like", verifyToken, likePost);
+
 export default router;
