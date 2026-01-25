9939fa064728439484ab361ccc95cc16beca5e13
debojit366 <debojitdas366@gmail.com>
2026-01-26 00:56:43 +0530

    feat(message) : add new controllers

--- Backend/models/messageModel.js
+++ Backend/models/messageModel.js
@@ -1,8 +1,18 @@
 const messageSchema = new mongoose.Schema({
     
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
-}, { timestamps: true });
+  senderId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User',
+    required: true },
+  receiverId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true },
+  text: { 
+    type: String,
+    required: true },
+}, 
+{ timestamps: true });
+
 
 export const Message = mongoose.model("Message", messageSchema);
